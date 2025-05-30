eea30573698c5d4a76ff17401a580b3ddd4f0bc0
Martin <martymoose21@gmail.com>
2025-05-30 10:58:33 -0400

    use movzx instead of movsx

--- phcvtps.asm
+++ phcvtps.asm
@@ -2,18 +2,19 @@ PUBLIC ConvertFP16ToFP32
 
 _TEXT SEGMENT
 
-; movsx       eax,cx  
+; movzx       eax,cx  
 ; movd        xmm0,eax  
 ; punpcklwd   xmm0,xmm0  
 ; pshufd      xmm0,xmm0,0  
 ; or 
 ; vpbroadcastw
+; apparently movsx is not good movzx
 ; CX = value
 ConvertFP16ToFP32 PROC
 
 push rbp
 mov rbp, rsp
-movsx eax, cx
+movzx eax, cx
 movd xmm2, eax
 vpbroadcastw xmm1, xmm2
 vcvtph2ps xmm0, xmm1
